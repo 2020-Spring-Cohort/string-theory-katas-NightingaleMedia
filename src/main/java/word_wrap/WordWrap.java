11ac33525501603bbbdf603cc62489ec9b2e916f
NightingaleMedia.AlbertMacbook <alsigman@gmail.com>
2020-02-16 16:34:58 -0500

    finished word wrap and refactored

--- src/main/java/word_wrap/WordWrap.java
+++ src/main/java/word_wrap/WordWrap.java
@@ -3,8 +3,19 @@ package word_wrap;
 public class WordWrap {
 
   public String wrapText(int lineLength, String unformatedString) {
-   
-    return "Cat \nin a\nhat";
-  }
+    StringBuilder sb = new StringBuilder();
+    int strLen = unformatedString.length();
 
+    int i = 0;
+    while (i < strLen) {
+      if(i % lineLength == 0 && i!=0) {
+        //trigger this every nth times
+        sb.append("\n");
+      }
+      sb.append(unformatedString.charAt(i));
+      i++;
+    }
+    return sb.toString();
+
+  }
 }
--- src/test/java/word_wrap/WordWrapTest.java
+++ src/test/java/word_wrap/WordWrapTest.java
@@ -7,9 +7,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class WordWrapTest {
     @Test
-    public void wrapTextTest() throws Exception {
+    public void wrapTextTest() {
         WordWrap underTest = new WordWrap();
         String result = underTest.wrapText(4, "Cat in a hat");
-        assertThat(result).isEqualTo("Cat \nin a\nhat");
+        assertThat(result).isEqualTo("Cat \nin a\n hat");
     }
+
+    @Test
+    public void newLineWithArgs() {
+        WordWrap underTest = new WordWrap();
+        String result = underTest.wrapText(3, "Cat in a hat");
+        assertThat(result).isEqualTo("Cat\n in\n a \nhat");
+    }
+
 }
